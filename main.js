20466214b23077e3d207f8e1ac5d87bf7fbd3902
Abdulkodir Yusupov <44323682+nvrbckdown@users.noreply.github.com>
2024-01-17 15:41:03 +0500

    initial commit

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ app.get('/config', getConfig);
 app.get('/domain', getDomain);
 app.get('/load-capability', getLoad);
 
-const port = 8080;
+const port = 80;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}...`);
 });
